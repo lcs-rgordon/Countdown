6a8b2608e199c1ec10cc43131a25b80f122a0a04
Russell Gordon <rgordon@lcs.on.ca>
2022-11-24 18:49:15 -0500

    Finished example

--- Countdown/main.swift
+++ Countdown/main.swift
@@ -7,5 +7,33 @@
 
 import Foundation
 
-print("Hello, World!")
+func countdown(from: Int, to: Int) {
+    
+    // Check for bad input
+    // 'from' must be greater than or equal to 'to' so a countdown can happen
+    if from < to {
+        print("Cannot countdown from \(from) to \(to)")
+        return
+    }
+        
+    if from == to {
+        // Reached base case, don't call function again
+        print("Blast off!")
+    } else {
+        // Print the current value of from
+        print(from)
+        
+        // We have not reached base case, so call function again
+        // BUT crucially, reduce the 'from' value by 1 so base case
+        // is eventually reached
+        countdown(from: from - 1, to: to)
+    }
+    
+}
 
+
+// This won't work
+countdown(from: 0, to: 10)
+
+// This will work!
+countdown(from: 10, to: 0)
